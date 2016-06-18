d0c91a9ec96fd596741e5952a021824f1a0ba90f
Prasanta Behera <pbehera@pbehera-mac.local>
2016-06-19 03:17:11 +0530

    Tower Of Hanoi, recursive solution

---
+++ c++/TOH-recursive.cpp
@@ -0,0 +1,22 @@
+//Tower of Hanoi - Recursive solution
+#include <iostream>
+#include <stdlib.h>
+
+void toh(int n, const char* from, const char* to, const char* aux) {
+  if (n < 1) {
+    std::cout << "Number of disks should be 1 or more" << std::endl;
+    return;
+  }
+  if (n > 1) {
+    toh(n - 1, from, aux, to);
+    std::cout << "move disk " << n << " from " << from << " to " << to << std::endl;
+    toh(n - 1, aux, to, from);
+  } else {
+    std::cout << "move disk " << n << " from " << from << " to " << to << std::endl;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  toh(2, "F", "T", "A");
+  return 0;
+}
