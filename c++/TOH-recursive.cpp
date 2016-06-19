2dc410417125d6588ff3b04e0deebcf2b96a0391
Prasanta Behera <pbehera@pbehera-mac.local>
2016-06-19 13:31:13 +0530

    TOH - minor changes

--- c++/TOH-recursive.cpp
+++ c++/TOH-recursive.cpp
@@ -3,20 +3,22 @@
 #include <stdlib.h>
 
 void toh(int n, const char* from, const char* to, const char* aux) {
-  if (n < 1) {
+  static int step = 0;
+  if (step == 0 && n < 1) {
     std::cout << "Number of disks should be 1 or more" << std::endl;
     return;
   }
-  if (n > 1) {
+  if (n > 0) {
     toh(n - 1, from, aux, to);
-    std::cout << "move disk " << n << " from " << from << " to " << to << std::endl;
+    std::cout << "Step # " << ++step << ": Move disk " << n << " from " << from << " to " << to << std::endl;
     toh(n - 1, aux, to, from);
-  } else {
-    std::cout << "move disk " << n << " from " << from << " to " << to << std::endl;
   }
 }
 
 int main(int argc, char const *argv[]) {
-  toh(4, "F", "T", "A");
+  std::cout << "Number of disks: ";
+  int c;
+  std::cin >> c;
+  toh(c, "F", "T", "A");
   return 0;
 }
